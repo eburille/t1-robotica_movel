0c0d1351b7b4d929982f8551ce0d39770ac13304
Enzo Burille <enzo_burille@hotmail.com>
2026-05-22 19:46:19 +0000

    first commit

---
+++ src/controlador.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+
+
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world T1 package\n");
+  return 0;
+}
